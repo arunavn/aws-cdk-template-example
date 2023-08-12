6f83c4b43cfcca97275d880c7efc4951430060b0
arunavn <arunavnarayan@gmail.com>
2023-08-12 17:19:16 +0530

    linting

--- cdk-app/services/sqs_service.py
+++ cdk-app/services/sqs_service.py
@@ -22,23 +22,24 @@ def create_basic_sqs(stack: Stack):
 
 
 def create_standard_queue(stack: Stack, kms_key: IKey | None = None):
-    """Creating and standard Queue, with Customer Managed 
+    """Creating and standard Queue, with Customer Managed
     KMS key / SSE-SQS encryption(Using L2 construct)"""
     app_config_obj = app_config.Config()
+    if kms_key is not None:
+        kms_key = None
     std_queue = sqs.Queue(
             stack,
             app_config_obj.generate_resource_id("standardQueue"),
-            data_key_reuse= Duration.seconds(600),
-            delivery_delay= Duration.seconds(1),
-            encryption= sqs.QueueEncryption.SQS_MANAGED,
+            data_key_reuse=Duration.seconds(600),
+            delivery_delay=Duration.seconds(1),
+            encryption=sqs.QueueEncryption.SQS_MANAGED,
             # encryption= sqs.QueueEncryption.KMS,
             # encryption_master_key= kms_key,
-            max_message_size_bytes= 262144,
-            queue_name= app_config_obj.generate_resource_name("standard-queue"),
-            retention_period= Duration.days(7),
-            visibility_timeout= Duration.minutes(2)
+            max_message_size_bytes=262144,
+            queue_name=app_config_obj.generate_resource_name(
+                    "standard-queue"
+                ),
+            retention_period=Duration.days(7),
+            visibility_timeout=Duration.minutes(2)
         )
     return std_queue
-
-
-
