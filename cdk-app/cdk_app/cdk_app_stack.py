f4d21015b40515a59de14c58d6a3d9eebf689c98
arunavn <arunavnarayan@gmail.com>
2023-08-13 17:55:02 +0530

    policy creation

--- cdk-app/cdk_app/cdk_app_stack.py
+++ cdk-app/cdk_app/cdk_app_stack.py
@@ -13,20 +13,22 @@ class CdkAppStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         # app_config_obj = app_config.Config()
         _ = sqs_service.create_basic_sqs(stack=self)
+        # Queue
         standard_queue = sqs_service.create_standard_queue(stack=self)
-        producer_bucket = s3_service.create_producer_bucket(
-                stack=self, dest_sqs=standard_queue
-            )
-        consume_policy = iam_service.create_sqs_policies(
+        sqs_consume_policy, sqs_purge_policy, _ = iam_service.create_sqs_policies(
                 stack=self,
                 queue=standard_queue,
                 queue_label="standardqueue"
             )
+        # Bucket
+        producer_bucket = s3_service.create_producer_bucket(
+                stack=self, dest_sqs=standard_queue
+            )
+        # Role
         lambda_role = iam_service.create_lambda_role(stack=self)
-        consume_policy.attach_to_role(lambda_role)
         # Granting queue permission to the role
-        standard_queue.grant_purge(lambda_role)
-        standard_queue.grant_consume_messages(lambda_role)
+        sqs_consume_policy.attach_to_role(lambda_role)
+        sqs_purge_policy.attach_to_role(lambda_role)
         # Granting s3 permissions to the role
         producer_bucket.grant_read_write(lambda_role)
         producer_bucket.grant_delete(lambda_role)
--- cdk-app/services/iam_service.py
+++ cdk-app/services/iam_service.py
@@ -26,17 +26,33 @@ def create_sqs_policies(
             stack: Stack,
             queue: sqs.Queue,
             queue_label: str
-        ) -> iam.Policy:
+        ) -> tuple[iam.Policy]:
     """Generate policies for sqs,
     can be attached to any other Principal
     """
     app_config_obj = app_config.Config()
     sqs_consume_policy = iam.Policy(
         stack,
-        app_config_obj.generate_resource_id(queue_label),
+        app_config_obj.generate_resource_id(queue_label + 'consume'),
         policy_name=app_config_obj.generate_resource_name(
                 queue_label
             )
     )
+    sqs_purge_policy = iam.Policy(
+        stack,
+        app_config_obj.generate_resource_id(queue_label + 'purge'),
+        policy_name=app_config_obj.generate_resource_name(
+                queue_label + 'purge'
+            )
+    )
+    sqs_send_policy = iam.Policy(
+        stack,
+        app_config_obj.generate_resource_id(queue_label + 'send'),
+        policy_name=app_config_obj.generate_resource_name(
+                queue_label + 'send'
+            )
+    )
     queue.grant_consume_messages(sqs_consume_policy)
-    return sqs_consume_policy
+    queue.grant_purge(sqs_purge_policy)
+    queue.grant_send_messages(sqs_send_policy)
+    return sqs_consume_policy, sqs_purge_policy, sqs_send_policy
